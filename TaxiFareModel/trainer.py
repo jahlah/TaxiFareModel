ba27e1464c07befcc9da9baea8a8b8963ba97730
Jakob Hohenstein <jakob-hohenstein@gmx.de>
2022-02-22 15:24:24 +0100

    changed ml_flow_test.py

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,16 +1,31 @@
 # imports
+from typing_extensions import Self
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
 from TaxiFareModel.data import get_data, clean_data
 from TaxiFareModel.utils import compute_rmse
+from memoized_property import memoized_property
 
+import mlflow
+from  mlflow.tracking import MlflowClient
+
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import Ridge
+
+
+Models = {
+    "LinearRegression": LinearRegression(),
+    "RandomForrest": RandomForestRegressor(),
+    "Ridge": Ridge()
+}
 
 class Trainer():
-    def __init__(self, X, y):
+
+    def __init__(self, X, y, model= None):
         """
             X: pandas DataFrame
             y: pandas Series
@@ -18,6 +33,8 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.experiment_name = "[DE] [Berlin] [jahlah] TaxiFare v1"
+        self.model = Models[model]
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -35,8 +52,10 @@ class Trainer():
         ], remainder="drop")
         pipe = Pipeline([
             ('preproc', preproc_pipe),
-            ('linear_model', LinearRegression())
+            (self.experiment_name, self.model)
         ])
+        self.mlflow_log_param("Model", "LinearRegression")
+
         self.pipeline = pipe
 
 
@@ -45,12 +64,39 @@ class Trainer():
         self.set_pipeline()
         self.pipeline.fit(self.X, self.y)
 
+
+
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
-        print(rmse)
+        #print(rmse)
         return rmse
+
+
+
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri("https://mlflow.lewagon.co/")
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 
 if __name__ == "__main__":
@@ -64,8 +110,17 @@ if __name__ == "__main__":
     # hold out
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     # train
-    trainer = Trainer(X_train, y_train)
-    trainer.set_pipeline()
+    trainer = Trainer(X_train, y_train, "LinearRegression")
     trainer.run()
     # evaluate
-    trainer.evaluate(X_test, y_test)
+    rmse = trainer.evaluate(X_test, y_test)
+    trainer.mlflow_log_param("model", trainer.model)
+    trainer.mlflow_log_metric("rmse", rmse)
+
+    #for model in []:
+    #    trainer = Trainer(X_train, y_train, model)
+    #    trainer.run()
+    #    rmse = trainer.evaluate(X_test, y_test)
+
+    #    trainer.mlflow_log_param("model", trainer.experiment_name)
+    #    trainer.mlflow_log_metric("rmse", rmse)
